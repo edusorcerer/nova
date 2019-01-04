136896259fedd30b0876cbbee3f311627ce35426
rickarof <desouzaricardoc@gmail.com>
2019-01-04 18:13:04 -0200

    added header

--- client/App.js
+++ client/App.js
@@ -1,21 +1,34 @@
-import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Header, FormLabel, FormInput, FormValidationMessage } from "react-native-elements";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working!</Text>
+      <View>
+        <Header
+          leftComponent={{ icon: "menu", color: "#fff" }}
+          centerComponent={{ text: "Balada Finder", style: { color: "#fff" } }}
+          rightComponent={{ icon: "home", color: "#fff" }}
+        />
+        <Text style={styles.title}>App de balada!</Text>
+        <Text style={styles.paragraph}>lorem ipsum</Text>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  view: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
+  },
+  title: {
+    color: "#666"
+  },
+  paragraph: {
+    color: "#000"
   }
-})
+});
