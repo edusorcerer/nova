bc1b08dcb19be9e05bf9202d014d623587849299
Eduardo <eduardofidera@gmail.com>
2019-01-14 19:39:38 -0200

    :sparkles: list places by neares

--- client/components/PlacesList.js
+++ client/components/PlacesList.js
@@ -19,10 +19,46 @@ class PlacesList extends Component {
     })
   }
 
+  calculateDistance = (lat1, lon1, lat2, lon2, unit) => {
+    const radlat1 = (Math.PI * lat1) / 180
+    const radlat2 = (Math.PI * lat2) / 180
+    const radlon1 = (Math.PI * lon1) / 180
+    const radlon2 = (Math.PI * lon2) / 180
+    const theta = lon1 - lon2
+    const radtheta = (Math.PI * theta) / 180
+    let dist =
+      Math.sin(radlat1) * Math.sin(radlat2) +
+      Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta)
+    dist = Math.acos(dist)
+    dist = (dist * 180) / Math.PI
+    dist = dist * 60 * 1.1515
+    if (unit == 'K') {
+      dist = dist * 1.609344
+    }
+    if (unit == 'N') {
+      dist = dist * 0.8684
+    }
+    return dist
+  }
+
   render() {
-    const { handlePlaceClick, props } = this
-    const { allPlacesQuery } = props
+    const { calculateDistance, handlePlaceClick, props } = this
+    const { address, allPlacesQuery } = props
     const { allPlaces } = allPlacesQuery
+
+    if (allPlaces) {
+      allPlaces.map(({ address: { geoCoordinates: { lat, longi } } }, i) => {
+        allPlaces[i]['distance'] = calculateDistance(
+          address.geoCoordinates.lat,
+          address.geoCoordinates.longi,
+          lat,
+          longi,
+          'K',
+        )
+      })
+
+      allPlaces.sort((a, b) => a.distance - b.distance)
+    }
 
     return allPlacesQuery.loading ? (
       <ActivityIndicator size="large" style={styles.loader} color="#009688" />
